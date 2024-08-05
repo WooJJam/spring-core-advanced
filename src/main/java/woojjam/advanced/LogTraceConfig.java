f5198591611510bc33bbd4e400e1c9c36e771378
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-08-05 22:24:03 +0900

    [section 2] 쓰레드 로컬 (ThreadLocal) - 쓰레드 로컬 동기화(적용)

--- src/main/java/woojjam/advanced/LogTraceConfig.java
+++ src/main/java/woojjam/advanced/LogTraceConfig.java
@@ -3,14 +3,14 @@ package woojjam.advanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import woojjam.advanced.trace.logtrace.FilterLogTrace;
 import woojjam.advanced.trace.logtrace.LogTrace;
+import woojjam.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-        return new FilterLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
